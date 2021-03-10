3eef435590f2b8a0382cb9ad70b604e4a84d6d8c
Diego Barbosa Sena <diegosena_93@hotmail.com>
2021-03-09 23:38:42 -0300

    aula 03

--- src/main/java/br/com/diego/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/diego/mvc/mudi/controller/HomeController.java
@@ -1,28 +1,24 @@
 package br.com.diego.mvc.mudi.controller;
 
 import br.com.diego.mvc.mudi.model.Order;
+import br.com.diego.mvc.mudi.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @GetMapping("/home")
     public String home(Model model) {
 
-        Order order = new Order();
-        order.setOrderName("Desbravando Java e Orientação a Objetos ");
-//        order.setNegotiatedValue(59.00);
-//        order.setDeliveryDate();
-        order.setOrderUrl("https://www.casadocodigo.com.br/products/livro-orientacao-objetos-java");
-        order.setOrderImage("https://cdn.shopify.com/s/files/1/0155/7645/products/orientacao-objetos-java-featured_large.png?v=1416402140");
-        order.setDescription("Aprenda Orientação a Objetos com Java de forma simples e efetiva! Você compilará e executará seu próprio programa em algumas horas. Por que Java? Essa é uma das linguagens de programação mais utilizadas em todo o mundo, com 20 anos e atualmente mais de 9 milhões de desenvolvedores. ");
-
-        List<Order> orders = Arrays.asList(order);
+        List<Order> orders = orderRepository.findAll();
         model.addAttribute("orders", orders);
 
         return "home";
--- src/main/java/br/com/diego/mvc/mudi/model/Order.java
+++ src/main/java/br/com/diego/mvc/mudi/model/Order.java
@@ -1,9 +1,16 @@
 package br.com.diego.mvc.mudi.model;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "`order`")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String orderName;
     private BigDecimal negotiatedValue;
@@ -11,6 +18,14 @@ public class Order {
     private String orderUrl;
     private String orderImage;
     private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getOrderName() {
         return orderName;
